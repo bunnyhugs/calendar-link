52f7b99ca9bd783810c66435bd7274315d90bfc5
A bunny <52647850+bunnyhugs@users.noreply.github.com>
2022-02-24 01:29:55 -0700

    Initial commit

---
+++ calendar-link-gen.js
@@ -0,0 +1,37 @@
+const calendarLinkElements = document.querySelectorAll("[data-calendar-title]");
+		calendarLinkElements.forEach(link => {
+			let title = link.getAttribute("data-calendar-title");
+			let start = link.getAttribute("data-calendar-start");
+			let end = link.getAttribute("data-calendar-end");
+			let duration = link.getAttribute("data-calendar-duration");
+			let linkText = link.getAttribute("data-calendar-linktext");
+			let description = link.getAttribute("data-calendar-description");
+			let location = link.getAttribute("data-calendar-location");
+			let url  = link.getAttribute("data-calendar-link");
+			
+			description = description.replace('{URLLink}', '<a href="' + url + '">' + linkText + '</a>');
+			location = location.replace("{URL}", url);
+			
+			let event = {
+				title: title,
+				start: start,
+				end: end,
+				duration: [duration, "hour"],
+				link: url,
+				linkText: linkText,
+				description: description,
+				location: location,
+				busy: true,
+				guests: [
+				]
+			};
+
+			let eventLinks = "<ul><li>" + title + "</li>";
+			eventLinks += "<li><a href=\"" + calendarLink.google(event) + "\" target=\"_blank\">Google</a></li>";
+			eventLinks += "<li><a href=\"" + calendarLink.office365(event) + "\" target=\"_blank\">Office365</a></li>";
+			eventLinks += "<li><a href=\"" + calendarLink.outlook(event) + "\" target=\"_blank\">Outlook</a></li>";
+			eventLinks += "<li><a href=\"" + calendarLink.ics(event) + "\" target=\"_blank\">iCal</a></li>";
+			eventLinks += "</ul>";
+			
+			link.innerHTML = eventLinks;
+		});
