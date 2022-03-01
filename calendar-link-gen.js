a17ea1fb74d909ff606d836f5f5ad0282a658afb
A bunny <52647850+bunnyhugs@users.noreply.github.com>
2022-03-01 15:13:35 -0700

    Update styling

--- calendar-link-gen.js
+++ calendar-link-gen.js
@@ -40,11 +40,11 @@ ready(function() {
 					]
 				};
 
-				let eventLinks = "<ul><li style=\"display: inline-block;\">" + title + "</li>";
-				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.google(event) + "\" target=\"_blank\">Google</a></li>";
-				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.office365(event) + "\" target=\"_blank\">Office365</a></li>";
-				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.outlook(event) + "\" target=\"_blank\">Outlook</a></li>";
-				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.ics(event) + "\" target=\"_blank\">iCal</a></li>";
+				let eventLinks = "<ul><li style=\"display: block; margin-top: 25px; font-weight: 600;\"><span style=\"font-weight: 600; display: inline-block; line-height: 1; padding: 8px 20px; margin-right: 10px; fill: #fff; color: #fff; font-size: 13px; text-transform: uppercase; letter-spacing: 1.4px; background-color: #cc2038; border-radius: 100px 100px 100px 100px; box-shadow: 0px 0px 27px 0px rgb(0 0 0 / 12%);\">Add event to your calendar</span><br /><br />" + title + "</li>";
+				eventLinks += "<li style=\"display: inline-block; margin-top: 10px; \"><a href=\"" + calendarLink.google(event) + "\" target=\"_blank\" class=\"googleCalendarLink calendarLink\">Google</a></li>";
+				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.office365(event) + "\" target=\"_blank\" class=\"office365CalendarLink calendarLink\">Office365</a></li>";
+				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.outlook(event) + "\" target=\"_blank\" class=\"outlookCalendarLink calendarLink\">Outlook</a></li>";
+				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.ics(event) + "\" target=\"_blank\" class=\"iCalCalendarLink calendarLink\">iCal</a></li>";
 				eventLinks += "</ul>";
 				
 				link.innerHTML = eventLinks;
