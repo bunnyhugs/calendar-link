2af8c242c0927988346f94085bd716dc4eae6d90
A bunny <52647850+bunnyhugs@users.noreply.github.com>
2022-03-01 15:31:16 -0700

    Make header optional

--- calendar-link-gen.js
+++ calendar-link-gen.js
@@ -21,6 +21,7 @@ ready(function() {
 				let location = link.getAttribute("data-calendar-location");
 				let url  = link.getAttribute("data-calendar-link");
 				let allDay  = link.getAttribute("data-calendar-allday");
+				let includeHeader  = link.getAttribute("data-calendar-includeHeader");
 				
 				description = description.replace('{URLLink}', '<a href="' + url + '">' + linkText + '</a>');
 				location = location.replace("{URL}", url);
@@ -40,7 +41,12 @@ ready(function() {
 					]
 				};
 
-				let eventLinks = "<ul><li style=\"display: block; margin-top: 25px; font-weight: 600;\"><span style=\"font-weight: 600; display: inline-block; line-height: 1; padding: 8px 20px; margin-right: 10px; fill: #fff; color: #fff; font-size: 13px; text-transform: uppercase; letter-spacing: 1.4px; background-color: #cc2038; border-radius: 100px 100px 100px 100px; box-shadow: 0px 0px 27px 0px rgb(0 0 0 / 12%);\">Add event to your calendar</span><br /><br />" + title + "</li>";
+				let eventLinks = "<ul>";
+				eventLinks += "<li style=\"display: block; margin-top: 25px; font-weight: 600;\">";
+				if (includeHeader) {
+					eventLinks += "<span style=\"font-weight: 600; display: inline-block; line-height: 1; padding: 8px 20px; margin-right: 10px; fill: #fff; color: #fff; font-size: 13px; text-transform: uppercase; letter-spacing: 1.4px; background-color: #cc2038; border-radius: 100px 100px 100px 100px; box-shadow: 0px 0px 27px 0px rgb(0 0 0 / 12%);\">Add event to your calendar</span><br /><br />";
+				}
+				eventLinks +=  title + "</li>";
 				eventLinks += "<li style=\"display: inline-block; margin-top: 10px; \"><a href=\"" + calendarLink.google(event) + "\" target=\"_blank\" class=\"googleCalendarLink calendarLink\">Google</a></li>";
 				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.office365(event) + "\" target=\"_blank\" class=\"office365CalendarLink calendarLink\">Office365</a></li>";
 				eventLinks += "<li style=\"display: inline-block;\"><a href=\"" + calendarLink.outlook(event) + "\" target=\"_blank\" class=\"outlookCalendarLink calendarLink\">Outlook</a></li>";
